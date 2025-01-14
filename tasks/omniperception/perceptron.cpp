06e0394151988c1333499d6a6a5361156916b6eb
konichan-7 <577994345@qq.com>
2025-01-14 21:20:21 +0800

    change the stucture

--- tasks/omniperception/perceptron.cpp
+++ tasks/omniperception/perceptron.cpp
@@ -15,40 +15,45 @@ Perceptron::Perceptron(
 : thread_pool_(4), detection_queue_(10), decider_(config_path), stop_flag_(false)
 {
   // 并行任务，读取并推理 USB 相机图像
-  auto parallel_infer = [&](io::USBCamera & cam, const std::string & config_path) {
-    auto_aim::YOLOV8 yolov8_parallel(config_path, false);
-    while (true) {
-      cv::Mat usb_img;
-      std::chrono::steady_clock::time_point ts;
+  auto parallel_infer =
+    [&](io::USBCamera & cam, auto_aim::YOLOV8 & yolov8_parallel, const std::string & config_path) {
+      while (true) {
+        cv::Mat usb_img;
+        std::chrono::steady_clock::time_point ts;
 
-      {
-        std::unique_lock<std::mutex> lock(mutex_);
-        if (stop_flag_) break;  // 检查是否需要退出
+        {
+          std::unique_lock<std::mutex> lock(mutex_);
+          if (stop_flag_) break;  // 检查是否需要退出
+        }
+
+        cam.read(usb_img, ts);
+
+        auto armors = yolov8_parallel.detect(usb_img);
+        if (!armors.empty()) {
+          auto delta_angle = decider_.delta_angle(armors, cam.device_name);
+
+          DetectionResult dr;
+          dr.armors = std::move(armors);
+          dr.timestamp = ts;
+          dr.delta_yaw = delta_angle[0];
+          dr.delta_pitch = delta_angle[1];
+
+          detection_queue_.push(dr);  // 推入线程安全队列
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(30));
       }
+    };
 
-      cam.read(usb_img, ts);
-
-      auto armors = yolov8_parallel.detect(usb_img);
-      if (!armors.empty()) {
-        auto delta_angle = decider_.delta_angle(armors, cam.device_name);
-
-        DetectionResult dr;
-        dr.armors = std::move(armors);
-        dr.timestamp = ts;
-        dr.delta_yaw = delta_angle[0];
-        dr.delta_pitch = delta_angle[1];
-
-        detection_queue_.push(dr);  // 推入线程安全队列
-      }
-      std::this_thread::sleep_for(std::chrono::milliseconds(30));
-    }
-  };
+  auto_aim::YOLOV8 yolov8_parallel1(config_path, false);
+  auto_aim::YOLOV8 yolov8_parallel2(config_path, false);
+  auto_aim::YOLOV8 yolov8_parallel3(config_path, false);
+  auto_aim::YOLOV8 yolov8_parallel4(config_path, false);
 
   // 将 4 个相机推理任务加入线程池
-  thread_pool_.add_task([&] { parallel_infer(usbcam1, config_path); });
-  thread_pool_.add_task([&] { parallel_infer(usbcam2, config_path); });
-  thread_pool_.add_task([&] { parallel_infer(usbcam3, config_path); });
-  thread_pool_.add_task([&] { parallel_infer(usbcam4, config_path); });
+  thread_pool_.add_task([&] { parallel_infer(usbcam1, yolov8_parallel1, config_path); });
+  thread_pool_.add_task([&] { parallel_infer(usbcam2, yolov8_parallel2, config_path); });
+  thread_pool_.add_task([&] { parallel_infer(usbcam3, yolov8_parallel3, config_path); });
+  thread_pool_.add_task([&] { parallel_infer(usbcam4, yolov8_parallel4, config_path); });
 
   tools::logger()->info("Perceptron initialized.");
 }
