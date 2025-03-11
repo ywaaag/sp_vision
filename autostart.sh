56608eb2248c4237fd98ee425465943d32cb4bc5
fishyu <fishyu1201@163.com>
2025-03-11 20:30:12 +0800

    update standard4.yaml

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,8 @@
 sleep 1
-cd ~/Desktop/sp_vision/
+cd ~/Desktop/sp_vision_25/
 screen \
     -L \
     -Logfile logs/$(date "+%Y-%m-%d_%H-%M-%S").screenlog \
     -d \
     -m \
-    bash -c "./build/standard configs/standard3.yaml"
+    bash -c "./build/standard configs/standard5.yaml"
