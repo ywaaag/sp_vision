157e968873e59b61adcc1f37c93ab3b2d2d3faa1
konichan-7 <2250624@tongji.edu.cn>
2025-03-15 05:51:16 +0800

    update params and autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,12 @@
-sleep 1
+sleep 5
 cd ~/Desktop/sp_vision_25/
-screen \
-    -L \
-    -Logfile logs/$(date "+%Y-%m-%d_%H-%M-%S").screenlog \
-    -d \
-    -m \
-    bash -c "./build/sentry configs/sentry.yaml"
+source /opt/ros/humble/setup.bash
+source /home/rm/Desktop/sp_msg_25/install/setup.bash
+export LD_LIBRARY_PATH=/opt/ros/humble/lib:/home/rm/Desktop/sp_msg_25/install/sp_msgs/lib:$LD_LIBRARY_PATH
+gnome-terminal -x bash -c "./build/sentry_multithread;exec bash"
+# screen \
+#     -L \
+#     -Logfile logs/$(date "+%Y-%m-%d_%H-%M-%S").screenlog \
+#     -d \
+#     -m \
+#     bash -c "export LD_LIBRARY_PATH=/opt/ros/humble/lib:/home/rm/Desktop/sp_msg_25/install/lib:$LD_LIBRARY_PATH; ./build/sentry_multithread configs/sentry.yaml"
