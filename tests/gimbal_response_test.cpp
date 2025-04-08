777ee8b658aab1f5dbad7d7e47cb3864b135233e
konichan-7 <577994345@qq.com>
2025-04-08 23:09:11 +0800

    change gimbal_response_test

--- tests/gimbal_response_test.cpp
+++ tests/gimbal_response_test.cpp
@@ -14,8 +14,8 @@ using namespace std::chrono_literals;
 
 const std::string keys =
   "{help h usage ? |                     | 输出命令行参数说明}"
-  "{delta-angle a  |         30          | yaw轴delta角}"
-  "{slice       s  |         250         | delta_angle的切片数}"
+  "{delta-angle a  |          8          | yaw轴delta角}"
+  "{circle      c  |         0.2         | delta_angle的切片数}"
   "{signal-mode m  |     triangle_wave   | 发送信号的模式}"
   "{@config-path   | configs/sentry.yaml | 位置参数，yaml配置文件路径 }";
 
@@ -24,7 +24,7 @@ int main(int argc, char * argv[])
   cv::CommandLineParser cli(argc, argv, keys);
   auto config_path = cli.get<std::string>(0);
   auto delta_angle = cli.get<double>("delta-angle");
-  auto slice = cli.get<double>("slice");
+  auto circle = cli.get<double>("circle");
   auto signal_mode = cli.get<std::string>("signal-mode");
   if (cli.has("help") || config_path.empty()) {
     cli.printMessage();
@@ -37,10 +37,10 @@ int main(int argc, char * argv[])
   io::CBoard cboard(config_path);
 
   auto init_yaw = 0;
+  double slice = circle * 100;  //切片数=周期*帧率
   auto dyaw = delta_angle / slice;
   double cmd_yaw = init_yaw;
   double error = 0;
-  double over_tune = 0, temp_tune;
   int count = 0;
   io::Command init_command{1, 0, 0, 0};
   cboard.send(init_command);
@@ -48,6 +48,7 @@ int main(int argc, char * argv[])
 
   io::Command command{0};
   io::Command last_command{0};
+
   while (!exiter.exit()) {
     nlohmann::json data;
     auto timestamp = std::chrono::steady_clock::now();
@@ -61,14 +62,11 @@ int main(int argc, char * argv[])
     if (signal_mode == "triangle_wave") {
       if (count == slice) {
         cmd_yaw = init_yaw;
-        error /= slice;
-        tools::logger()->info("average dynamic error is {:.4f} degree", error * 57.3);
         command = {1, 0, cmd_yaw / 57.3, 0};
         count = 0;
 
       } else {
         cmd_yaw += dyaw;
-        error += last_command.yaw - eulers[0];
         command = {1, 0, cmd_yaw / 57.3, 0};
         count++;
       }
@@ -80,21 +78,22 @@ int main(int argc, char * argv[])
       last_command = command;
       plotter.plot(data);
       std::this_thread::sleep_for(8ms);  //模拟自瞄100fps
-    } else if (signal_mode == "step") {
-      cmd_yaw = delta_angle;
-      command = {1, 0, cmd_yaw / 57.3, 0};
+    }
 
-      temp_tune = command.yaw - eulers[0];
-      if (temp_tune > over_tune) over_tune = temp_tune;
+    else if (signal_mode == "step") {
+      if (count == 300) {
+        cmd_yaw += delta_angle;
+        count = 0;
+      }
+      command = {1, 0, tools::limit_rad(cmd_yaw / 57.3), 0};
+      count++;
 
       cboard.send(command);
       data["cmd_yaw"] = command.yaw * 57.3;
+      data["last_cmd_yaw"] = last_command.yaw * 57.3;
       data["gimbal_yaw"] = eulers[0] * 57.3;
       last_command = command;
       plotter.plot(data);
-      if (std::abs(eulers[0] - command.yaw) < std::abs(command.yaw / 50)) {
-        tools::logger()->info("max over tune is {:.4f}", over_tune * 57.3);
-      }
       std::this_thread::sleep_for(8ms);  //模拟自瞄100fps
     }
   }
