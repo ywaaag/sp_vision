3052612ededde48a0d87ad248945d8b63c1f88ca
fishyu <fishyu1201@163.com>
2025-07-09 21:27:25 +0800

    use yolov8 for buff_detector

--- tasks/auto_buff/buff_detector.hpp
+++ tasks/auto_buff/buff_detector.hpp
@@ -8,10 +8,8 @@
 
 #include "buff_type.hpp"
 #include "tools/img_tools.hpp"
-
+#include "yolov8_buff.hpp"
 const int LOSE_MAX = 20;  // 丢失的阙值
-const cv::Scalar DETECTOR_COLOR_DEBUG = cv::Scalar(0, 255, 0);
-const cv::Scalar DETECTOR_COLOR_KEY = cv::Scalar(0, 0, 255);
 namespace auto_buff
 {
 class Buff_Detector
@@ -19,48 +17,24 @@ class Buff_Detector
 public:
   Buff_Detector(const std::string & config);
 
-  std::optional<PowerRune> detect(cv::Mat & bgr_img, bool debug = false);
+  std::optional<PowerRune> detect_24(cv::Mat & bgr_img);
+
+  std::optional<PowerRune> detect(cv::Mat & bgr_img);
+
+std::optional<PowerRune> detect_debug(cv::Mat & bgr_img, cv::Point2f v);
 
 private:
-  void handle_img(const cv::Mat & bgr_img, cv::Mat & handled_img);
+  void handle_img(const cv::Mat & bgr_img, cv::Mat & dilated_img);
 
-  std::optional<FanBlade> detect_fanblades(const cv::Mat & handled_img);
-  bool detect_fanblades_head(const cv::Mat & handled_img, std::vector<cv::Rect> & head_rects);
-  bool detect_fanblades_body(
-    const cv::Mat & handled_img, cv::Point2f body_box[4], int & fanblades_angle);
-
-  cv::Point2f detect_r_center(FanBlade & fanblade, const cv::Mat & handled_img);
+  cv::Point2f get_r_center(std::vector<FanBlade> & fanblades, cv::Mat & bgr_img);
 
   void handle_lose();
 
-  std::string enemy_color_;
-  int contrast_;
-  int brightness_;
-  int morphology_size_;
-  int dilate_size_;
-  // int canny_low_threshold_;
-  // int canny_high_threshold_;
-  int R_contours_min_area_;
-  int R_contours_max_area_;
-  int fanblades_head_contours_min_area_;
-  int fanblades_head_contours_max_area_;
-  int fanblades_body_contours_min_area_;
-  int fanblades_body_contours_max_area_;
-  int brightness_threshold_;
-  cv::Mat standard_fanblade;
-  cv::Size standard_fanblade_size;
-
-  cv::Point2f head_center, body_center;
-  double head_radius;
-
-  cv::Mat output;
-
+  YOLOV8_BUFF MODE_;
   Track_status status_;
   int lose_;  // 丢失的次数
   double lastlen_;
   std::optional<PowerRune> last_powerrune_ = std::nullopt;
-
-  bool debug_;
 };
 }  // namespace auto_buff
 #endif  // DETECTOR_HPP
